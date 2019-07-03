9fd169bf3bfaf7ea54bbbb6f22e99cda9e77fda4
Harsh Agarwal <harsh29092000@gmail.com>
2019-07-03 18:36:36 +0530

    Added 30 Days of Code.

---
+++ 30 Days of Code/15. linked list.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        if head == None:
+            head = Node(data)
+            return head
+        temp = head
+        while temp.next != None:
+            temp = temp.next
+        temp.next = Node(data)
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head)	  
